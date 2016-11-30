deeabe5583453383db5315062d0489e57f9e80b1
Kayque <kayque-willy@hotmail.com>
2016-11-30 16:46:21 +0000

    emergencia

--- application/modules/nota/controllers/Nota.php
+++ application/modules/nota/controllers/Nota.php
@@ -15,11 +15,38 @@ class Nota extends CI_Controller {
 		$this->load->model('turma/professor_model');
 		$this->load->model('avaliacao/avaliacao_model');
 		
+		//Consulta as turmas do professor
+		$consulta = new Turma_model();
+		$filtro['loginProfessor']=$_SESSION['login'];
+		$turmas=$consulta->select($filtro)->result();
+			
+		//Consulta as notas de cada turma
+		$data['notas'] = [];
+		foreach($turmas as $turma){
+			$consulta = new Professor_model();
+			$filtro['idTurma'] = $turma->id;
+			$filtro['idDisciplina'] = $turma->idDisciplina;
+			$filtro['loginProfessor'] = $turma->loginProfessor;
+				
+			//Adiciona as notas e a turma no vetor
+			$notas['turma'] = $turma;
+			$notas['notas'] = $consulta->listar_notas($filtro)->result();
+			$data['notas'][] = $notas;
+		}
+			
+		//var_dump($data['notas'][0]);
+			
+		//Carrega a view 
+		$this->load->view('listarNota', $data);
+		
+	}
+	
+	//USA ESSE METODO DIÓGENES
+	public function historico(){
+		
+			
 		if ($_SESSION['tipoUsuario'] === 'aluno'){
-			$consulta = new Turma_model();
-		} else {
-			//Consulta as turmas do professor
-			$consulta = new Turma_model();
+			$consulta = new Nota_model();
 			$filtro['login']=$_SESSION['login'];
 			$turmas=$consulta->select($filtro)->result();
 			
@@ -37,11 +64,10 @@ class Nota extends CI_Controller {
 				$data['notas'][] = $notas;
 			}
 			
-			var_dump($data['notas'][0]);
-			
 			//Carrega a view 
 			$this->load->view('listarNota', $data);
-		}
+			}
+		
 	}
 
 }
